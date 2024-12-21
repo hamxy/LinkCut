0f645adc8d6d73e02d8a2d2d76a601bde002840c
hamxy <zylkadamian@gmail.com>
2024-12-21 21:39:44 +0100

    Initialise backend

---
+++ backend/linkcut/src/main/java/pl/linkcut/LinkcutApplication.java
@@ -0,0 +1,13 @@
+package pl.linkcut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkcutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkcutApplication.class, args);
+	}
+
+}
---
+++ backend/linkcut/src/test/java/pl/linkcut/LinkcutApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.linkcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
