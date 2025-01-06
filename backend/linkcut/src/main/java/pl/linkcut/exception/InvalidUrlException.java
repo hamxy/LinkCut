3ba9f2045c13ae5254352afce206320ef8a91b3c
hamxy <zylkadamian@gmail.com>
2025-01-06 15:25:11 +0100

    Add InvalidUrlException

---
+++ backend/linkcut/src/main/java/pl/linkcut/exception/InvalidUrlException.java
@@ -0,0 +1,16 @@
+package pl.linkcut.exception;
+
+public class InvalidUrlException extends RuntimeException{
+
+    public InvalidUrlException(String message) {
+        super(message);
+    }
+
+    public InvalidUrlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidUrlException(Throwable cause) {
+        super(cause);
+    }
+}
