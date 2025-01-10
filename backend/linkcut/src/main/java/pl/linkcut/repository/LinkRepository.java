b7def948cbeecd6709945593abad29a2a430ad76
hamxy <zylkadamian@gmail.com>
2025-01-10 11:44:46 +0100

    Add LinkRepository class

---
+++ backend/linkcut/src/main/java/pl/linkcut/repository/LinkRepository.java
@@ -0,0 +1,11 @@
+package pl.linkcut.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.linkcut.entity.Link;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+    Link findByShortenedLink(String shortenedLink);
+    void deleteExpiredLinks();
+
+}
