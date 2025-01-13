738386f7067b11352b9411d3f1a1ae1a35e3603d
hamxy <zylkadamian@gmail.com>
2025-01-13 14:57:54 +0100

    Add deleteAllByExpirationDateBefore method

--- backend/linkcut/src/main/java/pl/linkcut/repository/LinkRepository.java
+++ backend/linkcut/src/main/java/pl/linkcut/repository/LinkRepository.java
@@ -3,9 +3,12 @@ package pl.linkcut.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.linkcut.entity.Link;
 
+import java.time.LocalDate;
+
 public interface LinkRepository extends JpaRepository<Link, Long> {
 
     Link findByShortenedLink(String shortenedLink);
-//    void deleteExpiredLinks();
+
+    void deleteAllByExpirationDateBefore(LocalDate date);
 
 }
