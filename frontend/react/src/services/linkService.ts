614cbcfd9357aa00917012dea8d487dc68e44f4b
hamxy <zylkadamian@gmail.com>
2025-01-31 09:58:11 +0100

    Move error handling to a separate file

--- frontend/react/src/services/linkService.ts
+++ frontend/react/src/services/linkService.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../config/axiosConfig";
 import { LinkRequest, LinkResponse } from "../dto/linkDto";
 import { handleApiError } from "../exceptions/apiErrorHandler";
 
-export const createLink = async (data: LinkRequest): Promise<LinkResponse> => {
+export const createLinkService = async (data: LinkRequest): Promise<LinkResponse> => {
     try {
         const response = await axiosInstance.post<LinkResponse>("/shorten", data);
         return response.data;
